d8eedd9ba1bd2f3a8ce499afcdbebac3bb712474
ShishantG87 <shishantbohora44@gmail.com>
2026-03-10 23:20:03 -0400

    add grid system and mouse interaction

--- grid.py
+++ grid.py
@@ -2,6 +2,9 @@ import pygame
 
 WHITE = (255,255,255)
 GREY = (200,200,200)
+BLACK = (0,0,0)
+GREEN = (0,255,0)
+RED = (255,0,0)
 
 
 class Node:
@@ -9,10 +12,31 @@ class Node:
     def __init__(self, row, col, width):
         self.row = row
         self.col = col
-        self.x = row * width
-        self.y = col * width
+        self.x = col * width
+        self.y = row * width
         self.color = WHITE
         self.width = width
+        
+    def is_start(self):
+        return self.color == GREEN
+
+    def is_end(self):
+        return self.color == RED
+
+    def is_wall(self):
+        return self.color == BLACK
+
+    def reset(self):
+        self.color = WHITE
+
+    def make_start(self):
+        self.color = GREEN
+
+    def make_end(self):
+        self.color = RED
+
+    def make_wall(self):
+        self.color = BLACK
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
@@ -51,3 +75,14 @@ def draw(win, grid, rows, width):
 
     draw_grid(win, rows, width)
     pygame.display.update()
+
+
+def get_clicked_pos(pos, rows, width):
+
+    gap = width // rows
+    x, y = pos
+
+    row = y // gap
+    col = x // gap
+
+    return row, col
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from grid import make_grid, draw
+from grid import make_grid, draw, get_clicked_pos
 
 WIDTH = 800
 ROWS = 40
@@ -9,17 +9,57 @@ pygame.display.set_caption("Pathfinding Visualizer")
 
 
 def main():
+
     pygame.init()
 
     grid = make_grid(ROWS, WIDTH)
 
+    start = None
+    end = None
+
     running = True
     while running:
+
         draw(WIN, grid, ROWS, WIDTH)
 
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 running = False
+
+            # LEFT CLICK
+            if pygame.mouse.get_pressed()[0]:
+
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, WIDTH)
+
+                node = grid[row][col]
+
+                if not start and node != end:
+                    start = node
+                    start.make_start()
+
+                elif not end and node != start:
+                    end = node
+                    end.make_end()
+
+                elif node != start and node != end:
+                    node.make_wall()
+
+            # RIGHT CLICK
+            if pygame.mouse.get_pressed()[2]:
+
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, WIDTH)
+
+                node = grid[row][col]
+                node.reset()
+
+                if node == start:
+                    start = None
+
+                if node == end:
+                    end = None
 
     pygame.quit()
 
